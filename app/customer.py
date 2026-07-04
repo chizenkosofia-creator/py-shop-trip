837ae87df4face166e63982e8852f5ac10218cb6
Sofia <chizenko.sofia@gmail.com>
2026-07-04 21:38:07 +0300

    solution4

--- app/customer.py
+++ app/customer.py
@@ -1,4 +1,5 @@
 from app.car import Car
+from app.shop import Shop
 
 
 class Customer:
@@ -8,3 +9,22 @@ class Customer:
         self.location = data["location"]
         self.money = data["money"]
         self.car = Car(data["car"])
+
+    def calculate_shopping_costs(self, shop: Shop) -> dict:
+        costs = {}
+        total_products_cost = 0.0
+
+        for product, quantity in self.product_cart.items():
+            product_cost = quantity * shop.products[product]
+            if product_cost.is_integer():
+                costs[product] = int(product_cost)
+            else:
+                costs[product] = product_cost
+            total_products_cost += product_cost
+
+        if total_products_cost.is_integer():
+            costs["total"] = int(total_products_cost)
+        else:
+            costs["total"] = total_products_cost
+
+        return costs
--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
-import json
 import datetime
+import json
 from pathlib import Path
 
 from app.customer import Customer
@@ -14,9 +14,6 @@ def shop_trip() -> None:
     customers = [Customer(data) for data in config["customers"]]
     shops = [Shop(data) for data in config["shops"]]
     fuel_price = config["FUEL_PRICE"]
-    distance = 0
-    best_shop = None
-    price_ride = 0
 
     for index, customer in enumerate(customers):
         if index > 0:
@@ -24,24 +21,21 @@ def shop_trip() -> None:
 
         print(f"{customer.name} has {customer.money} dollars")
 
-        min_total_cost = 1000000
+        min_total_cost = 1000000.0
         best_shop = None
+        best_shop_costs = {}
 
         for shop in shops:
             distance = (
-                               (customer.location[0] - shop.location[0]) ** 2
-                               + (customer.location[1] - shop.location[1]) ** 2
-                       ) ** 0.5
+                (customer.location[0] - shop.location[0]) ** 2
+                + (customer.location[1] - shop.location[1]) ** 2
+            ) ** 0.5
             price_ride = (
-                    customer.car.fuel_consumption / 100 * distance * fuel_price
+                customer.car.fuel_consumption / 100 * distance * fuel_price
             )
 
-            current_total_cost = (
-                    price_ride * 2
-                    + customer.product_cart["milk"] * shop.products["milk"]
-                    + customer.product_cart["bread"] * shop.products["bread"]
-                    + customer.product_cart["butter"] * shop.products["butter"]
-            )
+            shop_costs = customer.calculate_shopping_costs(shop)
+            current_total_cost = (price_ride * 2) + shop_costs["total"]
 
             print(
                 f"{customer.name}'s trip to the {shop.name} "
@@ -51,9 +45,9 @@ def shop_trip() -> None:
             if current_total_cost < min_total_cost:
                 min_total_cost = current_total_cost
                 best_shop = shop
+                best_shop_costs = shop_costs
 
         if min_total_cost > customer.money:
-
             print(
                 f"{customer.name} doesn't have enough money "
                 f"to make a purchase in any shop"
@@ -64,45 +58,23 @@ def shop_trip() -> None:
 
             date_str = datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")
 
-            milk_cost = (
-                    customer.product_cart["milk"] * best_shop.products["milk"]
-            )
-            bread_cost = (
-                    customer.product_cart["bread"] * best_shop.products["bread"]
-            )
-            butter_cost = (
-                    customer.product_cart["butter"] * best_shop.products["butter"]
-            )
-            total_prod_cost = (
-                    customer.product_cart["milk"] * best_shop.products["milk"]
-                    + customer.product_cart["bread"] * best_shop.products["bread"]
-                    + customer.product_cart["butter"] * best_shop.products["butter"]
-            )
-
             print(f"{customer.name} rides to {best_shop.name}\n")
             print(f"Date: {date_str}")
             print(f"Thanks, {customer.name}, for your purchase!")
-            print(f"You have bought:")
+            print("You have bought:")
             print(
                 f"{customer.product_cart['milk']} milks for "
-                f"{int(milk_cost) if milk_cost.is_integer() else milk_cost} dollars"
+                f"{best_shop_costs['milk']} dollars"
             )
             print(
                 f"{customer.product_cart['bread']} breads for "
-                f"{int(bread_cost) if bread_cost.is_integer() 
-                else bread_cost} dollars"
+                f"{best_shop_costs['bread']} dollars"
             )
             print(
                 f"{customer.product_cart['butter']} butters for "
-                f"{int(butter_cost) if butter_cost.is_integer() 
-                else butter_cost} dollars"
+                f"{best_shop_costs['butter']} dollars"
             )
-            print(
-                f"Total cost is "
-                f"{int(total_prod_cost) if total_prod_cost.is_integer() 
-                else total_prod_cost} "
-                f"dollars"
-            )
+            print(f"Total cost is {best_shop_costs['total']} dollars")
             print("See you again!\n")
             print(f"{customer.name} rides home")
             print(f"{customer.name} now has {customer.money:.2f} dollars")
